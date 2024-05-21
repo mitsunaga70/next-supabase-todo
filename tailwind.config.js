d2280071f3b16745bb807d266dc9c2654ae053b4
mitsunaga <mitsunaga@fieldweb.co.jp>
2024-05-21 17:31:00 +0900

    #

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "hsl(var(--background))",
-        foreground: "hsl(var(--foreground))",
-        btn: {
-          background: "hsl(var(--btn-background))",
-          "background-hover": "hsl(var(--btn-background-hover))",
-        },
+        'black-rgba': 'rgba(0, 0, 0, 0.3)',
       },
     },
   },
   plugins: [],
-};
+}
